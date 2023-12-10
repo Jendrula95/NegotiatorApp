2106f91b43cf4d4bad2e01df8bdecd5a85d06253
Jendrula95 <Jendrula95@hotmail.com>
2023-12-10 16:56:48 +0100

    Add project files.

---
+++ Negotiator/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿namespace Negotiator.Models
+{
+    public class Products
+    {
+        public long Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+
+        public decimal Price { get; set; }
+
+
+        public string? Category { get; set; }
+
+    }
+}
